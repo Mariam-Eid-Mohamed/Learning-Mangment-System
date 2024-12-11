25f230b30ce80523a80cba19c441e02dd3a89d53
mariameid <mariameid210@gmail.com>
2024-12-11 22:35:46 +0200

    Initial commit

---
+++ src/main/java/com/lms/learningmangmentsystem/LearningMangmentSystemApplication.java
@@ -0,0 +1,14 @@
+package com.lms.learningmangmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class LearningMangmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningMangmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lms/learningmangmentsystem/LearningMangmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.learningmangmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningMangmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
